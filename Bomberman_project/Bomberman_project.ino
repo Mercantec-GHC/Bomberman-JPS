37091d13a8911bbf101c14cfc71e3bfd9e4b2cfe
silasbillum <stilhofsvej46@gmail.com>
2025-04-29 08:15:10 +0200

    sends moves to the mqtt

--- Bomberman_project/Bomberman_project.ino
+++ Bomberman_project/Bomberman_project.ino
@@ -19,23 +19,11 @@ PubSubClient client(wifiClient);
 MKRIoTCarrier carrier;
 
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect("ArduinoMKR", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT broker");
-      client.subscribe(mqttTopic);  // Subscribe to the topic
-    } else {
-      Serial.print("Failed, status code: ");
-      Serial.println(client.state());
-      delay(5000);
-    }
-  }
-}
 
 
 float x, y, z;
 float tiltThreshold = 0.3;
+
 void setup() {
   Serial.begin(9600);
   IMU.begin();
@@ -43,52 +31,75 @@ void setup() {
     Serial.println("Failed to initialize IMU!");
     while (1);
 }
+
+Serial.println("Connecting to WiFi...");
+   WiFi.begin(ssid, password);  // Use your Wi-Fi credentials
+   while (WiFi.status() != WL_CONNECTED) {
+     delay(1000);
+     Serial.print(".");
+   }
+   Serial.println("Connected to WiFi");
+ 
+   client.setServer(mqtt_server, mqtt_port);  // Set the MQTT server and port
   
 }
 
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);  // Use your Wi-Fi credentials
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("Connected to WiFi");
-
-  client.setServer(mqtt_server, mqtt_port);  // Set the MQTT server and port
-
-
-    
+    void reconnect() {
+   while (!client.connected()) {
+     Serial.println("Attempting MQTT connection...");
+     if (client.connect("ArduinoMKR", mqtt_username, mqtt_password)) {
+       Serial.println("Connected to MQTT broker");
+       client.subscribe(mqttTopic);  // Subscribe to the topic
+     } else {
+       Serial.print("Failed, status code: ");
+       Serial.println(client.state());
+       delay(5000);
+     }
+   }
+ }
     
 
 
 void loop() {
+  
+if (!client.connected()) {
+  reconnect();
+  }
+  client.loop();
+
+  
+  delay(700);
+
  if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x,y,z);   
 
      if (y > tiltThreshold) {
       Serial.println("MOVE LEFT");
+      String Message = "Move Left";
+      client.publish("game/status", Message.c_str());
     } else if (y < -tiltThreshold) {
       Serial.println("MOVE RIGHT");
+      String Message = "Move Right";
+      client.publish("game/status", Message.c_str());
     } else if (x > tiltThreshold) {
       Serial.println("MOVE UP");
+      String Message = "Move Up";
+      client.publish("game/status", Message.c_str());
     } else if (x < -tiltThreshold) {
       Serial.println("MOVE DOWN");
+      String Message = "Move Down";
+      client.publish("game/status", Message.c_str());
     }else {
       Serial.println("IDLE");
+      String Message = "Idle";
+      client.publish("game/status", Message.c_str());
     }    
   }
-  delay(200);
+  
   
 
-  if (!client.connected()) {
-  reconnect();
-  }
-  client.loop();
-
-  String Message = "Hello World!";
-  client.publish("game/status", Message.c_str());
-  delay(30000);
+  
   
 
 }
