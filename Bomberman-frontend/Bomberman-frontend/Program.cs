5c6a7b0d826b00978bbe834849baf2adbcba44b0
JhM20P <jmikkelsen40@outlook.com>
2025-05-08 10:18:47 +0200

    implmented basic leaderboard test in frontend

--- Bomberman-frontend/Bomberman-frontend/Program.cs
+++ Bomberman-frontend/Bomberman-frontend/Program.cs
@@ -3,10 +3,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using BombermanGame.Source.Engine.Input;
+using MudBlazor.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<PlayerInput>();
+builder.Services.AddHttpClient();
+builder.Services.AddMudServices();
 builder.Services.AddHostedService<MqttClientService>();
 builder.Services.AddSingleton<MqttClientService>();
 
