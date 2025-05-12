131ae751193fd03602535b1cdb33b0a68273074b
JhM20P <jmikkelsen40@outlook.com>
2025-05-12 07:33:38 +0200

    copied the game over to the new solution

---
+++ Bomberman-project/BombermanGame/Source/Engine/Map/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BombermanGame.Source.Engine.Map
+{
+    public class Tile
+    {
+        public Texture2D Texture { get; set; }
+        public Rectangle Position { get; set; }
+        public bool IsWall { get; set; }
+
+        public Tile(Texture2D texture, Rectangle position, bool isWall)
+        {
+            Texture = texture;
+            Position = position;
+            IsWall = isWall;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
---
+++ Bomberman-project/BombermanGame/Source/World.cs
@@ -0,0 +1,45 @@
+﻿using BombermanGame.Source.Engine;
+using BombermanGame.Source.Engine.Input;
+using BombermanGame.Source.Engine.Map;
+using BombermanGame.Source.Engine.PlayerManager;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BombermanGame.Source
+{
+    public class World
+    {
+        public Player _player;
+        private Tilemap _tilemap;
+
+        private Texture2D _tileTexture;
+        private Texture2D _wallTexture;
+
+        public void Load(ContentManager content)
+        {
+            _wallTexture = content.Load<Texture2D>("2d/Wall");
+            _tileTexture = content.Load<Texture2D>("2d/Tile");
+            _player = new Player(null, new Vector2(50, 50));
+
+            _tilemap = new Tilemap(10, 10, _tileTexture, _wallTexture);
+        }
+        public void Update(PlayerInput input)
+        {
+            _player.Update(input.MoveDirection, _tilemap);
+        }
+
+        public void SetPlayerTextures(Texture2D[] textures)
+        {
+            if (_player != null)
+                _player.SetTexture(textures[0]); // Start with first frame
+        }
+
+        public void Draw()
+        {
+            _tilemap.Draw(Globals.spriteBatch);
+            _player.Draw(Globals.spriteBatch);
+
+        }
+    }
+}
