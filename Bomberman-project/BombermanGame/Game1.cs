ddaee3e42d4786f312787f234910a2d86bdf565c
silasbillum <stilhofsvej46@gmail.com>
2025-05-12 18:51:02 +0200

    updated bomb explosion

--- Bomberman-project/BombermanGame/Game1.cs
+++ Bomberman-project/BombermanGame/Game1.cs
@@ -7,6 +7,7 @@ using BombermanGame.Source.Engine;
 using BombermanGame.Source.Engine.Input;
 using SharpDX.Direct2D1;
 using System.Collections.Generic;
+using System;
 
 
 
@@ -18,12 +19,22 @@ namespace BombermanGame
         private PlayerInput _input;
         List<Bomb> activeBombs = new List<Bomb>();
         Texture2D[] bombTextures;
+        
+        List<Vector2> activeExplosions = new List<Vector2>();
 
+        Texture2D explosionCenter;
+        Texture2D explosionHorizontal;
+        Texture2D explosionVertical;
+
+        Dictionary<Vector2, int> explosionTimers = new Dictionary<Vector2, int>();
+        int explosionDuration = 30;
+        int tileSize = 70;
         World world;
 
         Texture2D[] runningTextures;
 
-
+        double bombCooldown = 2000; 
+        double timeSinceLastBomb = 0;
         int counter;
         int activateFrame;
 
@@ -69,12 +80,16 @@ namespace BombermanGame
             runningTextures[0] = Content.Load<Texture2D>("2d/Animation/PlayerAnimation/Human");
             runningTextures[1] = Content.Load<Texture2D>("2d/Animation/PlayerAnimation/Human2nd");
 
-            bombTextures = new Texture2D[10];
+            bombTextures = new Texture2D[8];
 
-            for (int i = 0; i < 10; i++) 
+            for (int i = 0; i < 8; i++) 
             {
                 bombTextures[i] = Content.Load<Texture2D>($"2d/Animation/Bomb/Bomb{i + 1}");
-            }            
+            }
+
+            explosionCenter = Content.Load<Texture2D>("2d/Animation/Bomb/Bomb8");
+            explosionHorizontal = Content.Load<Texture2D>("2d/Animation/Bomb/Bomb9");
+            explosionVertical = Content.Load<Texture2D>("2d/Animation/Bomb/Bomb10");
 
             world = new World();
             world.Load(Content);
@@ -89,7 +104,7 @@ namespace BombermanGame
 
             _input.HandleKeyboardInput();
             world.Update(_input);
-          
+            timeSinceLastBomb += gameTime.ElapsedGameTime.TotalMilliseconds;
 
             counter++;
             if (counter > 25)
@@ -106,23 +121,66 @@ namespace BombermanGame
                 world.SetPlayerTextures(new Texture2D[] { runningTextures[activateFrame] });
             }
 
-            if (_input.BombPlaced)
+            if (_input.BombPlaced && timeSinceLastBomb >= bombCooldown)
             {
                 Vector2 playerPos = world._player.Position;
                 activeBombs.Add(new Bomb(playerPos));
+                timeSinceLastBomb = 0;
             }
 
-            for(int i = activeBombs.Count - 1; i >= 0; i--)
+            for (int i = activeBombs.Count - 1; i >= 0; i--)
             {
                 activeBombs[i].Update(bombTextures);
-                if(activeBombs[i].IsFinsihed)
+                if (activeBombs[i].IsFinsihed)
                 {
+                    CreateExplosion(activeBombs[i].Position);
                     activeBombs.RemoveAt(i);
+                }
+            }
+            for (int i = activeExplosions.Count - 1; i >= 0; i--)
+            {
+                Vector2 pos = activeExplosions[i];
+
+                if (explosionTimers.ContainsKey(pos))
+                {
+                    explosionTimers[pos]++;
+
+                    if (explosionTimers[pos] > explosionDuration)
+                    {
+                        explosionTimers.Remove(pos);
+                        activeExplosions.RemoveAt(i);
+                    }
+                }
+                else
+                {
+                    activeExplosions.RemoveAt(i); // extra safety fallback
                 }
             }
             _input.ResetActions();
 
             base.Update(gameTime);
+        }
+
+        private void CreateExplosion(Vector2 centerPos)
+        {
+            void AddExplosionTile(Vector2 pos)
+            {
+                if (!explosionTimers.ContainsKey(pos))
+                {
+                    activeExplosions.Add(pos);
+                    explosionTimers[pos] = 0;
+                }
+            }
+
+            AddExplosionTile(centerPos); // Center
+
+            for (int i = 1; i <= 3; i++)
+            {
+                AddExplosionTile(centerPos + new Vector2(tileSize * i, 0));  // Right
+                AddExplosionTile(centerPos + new Vector2(-tileSize * i, 0)); // Left
+                AddExplosionTile(centerPos + new Vector2(0, tileSize * i));  // Down
+                AddExplosionTile(centerPos + new Vector2(0, -tileSize * i)); // Up
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -138,7 +196,25 @@ namespace BombermanGame
             {
                 bomb.Draw(Globals.spriteBatch, bombTextures);
             }
-            
+
+            Vector2 center = Vector2.Zero;
+            if (activeExplosions.Count > 0)
+                center = activeExplosions[0]; // Assume first added is center
+
+            foreach (var pos in activeExplosions)
+            {
+                Texture2D textureToDraw;
+
+                if (pos == center)
+                    textureToDraw = explosionCenter;
+                else if (Math.Abs(pos.Y - center.Y) < 1)
+                    textureToDraw = explosionHorizontal;
+                else
+                    textureToDraw = explosionVertical;
+
+                Globals.spriteBatch.Draw(textureToDraw, new Rectangle((int)pos.X, (int)pos.Y, tileSize, tileSize), Color.White);
+            }
+
             Globals.spriteBatch.End();
             
             
