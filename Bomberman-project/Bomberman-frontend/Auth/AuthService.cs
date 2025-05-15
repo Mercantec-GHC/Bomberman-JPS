4c2bd3098c3baee3040f674eff6ecaa2f49a2651
JhM20P <jmikkelsen40@outlook.com>
2025-05-15 14:02:24 +0200

    basic authentation and authorization

---
+++ Bomberman-project/Bomberman-frontend/Auth/AuthService.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using Blazored.LocalStorage;
+using DomainModels;
+using DomainModels.DTO;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Session;
+using Microsoft.IdentityModel.JsonWebTokens;
+using static System.Net.WebRequestMethods;
+using System.Net.Http.Headers;
+
+public delegate void AuthenticationChanged(AuthenticationState authenticationState);
+
+public class AuthService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+
+    public AuthService(HttpClient httpClient, ILocalStorageService localStorage)
+    {
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+    }
+
+    public async Task SetAuthorHeader()
+    {
+        var token = await _localStorage.GetItemAsync<string>("authToken");
+        if (token != null)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+
+    public async Task<TokenResponse> Login(LoginDTO loginDTO)
+    {
+        var response = await _httpClient.PostAsJsonAsync("/api/Player/login", loginDTO);
+        if (response.IsSuccessStatusCode)
+        {
+            var token = await response.Content.ReadFromJsonAsync<TokenResponse>();
+            await _localStorage.SetItemAsync("authToken", token.Token);
+            await _localStorage.SetItemAsync("refreshToken", token.RefreshToken);
+            return token;   
+        }
+        else
+        {
+            throw new Exception("Login failed");
+        }
+    }
+
+    public async Task<HttpResponseMessage> Register(CreatePlayerDTO request)
+    {
+        var response = await _httpClient.PostAsJsonAsync("/api/Player", request);
+        return response;
+    }
+
+    public async Task<T?> GetFrom<T>(string url)
+    {
+        await SetAuthorHeader();
+        return await _httpClient.GetFromJsonAsync<T?>(url);
+    }
+
+    public HttpClient GetHttpClient() => _httpClient;
+}
+
--- Bomberman-project/Bomberman-frontend/Program.cs
+++ Bomberman-project/Bomberman-frontend/Program.cs
@@ -1,6 +1,8 @@
 using Bomberman_frontend.Components;
 using DomainModels;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,21 @@ builder.Services.AddHttpClient();
 builder.Services.AddMudServices();
 builder.Services.AddHostedService<MqttClientService>();
 builder.Services.AddSingleton<MqttClientService>();
+
+
+// Authentication and authorization
+IConfiguration configuration = builder.Configuration;
+var baseUrl = configuration["BaseUrl"] ??
+                Environment.GetEnvironmentVariable("BaseUrl");
+builder.Services.AddHttpClient<AuthService>("api", options =>
+{
+    options.BaseAddress = new Uri(baseUrl);
+});
+builder.Services.AddScoped<AuthenticationStateProvider, AuthProvider>();
+builder.Services.AddAuthenticationCore();
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddBlazoredLocalStorage();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -30,6 +47,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
